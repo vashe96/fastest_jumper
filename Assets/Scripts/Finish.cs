a621b29d5e47acae31868789dccec8a8c26dc040
Valentyn Shemchuk <valentinshemchuk8@gmail.com>
2020-06-07 16:50:32 +0300

    Level progression system done.

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -4,12 +4,71 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
-{   
+{
+    string currentScene;
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
         {
+            SetProgression();
             SceneManager.LoadScene(1);
+        }
+    }
+
+    void SetProgression()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+
+        switch(currentScene)
+        {
+            case "Level 1":
+                if (PlayerPrefs.GetInt("LevelPassed") < 1)
+                    PlayerPrefs.SetInt("LevelPassed", 1);
+                break;
+            case "Level 2":
+                if (PlayerPrefs.GetInt("LevelPassed") < 2)
+                    PlayerPrefs.SetInt("LevelPassed", 2);
+                break;
+            case "Level 3":
+                if (PlayerPrefs.GetInt("LevelPassed") < 3)
+                    PlayerPrefs.SetInt("LevelPassed", 3);
+                break;
+            case "Level 4":
+                if (PlayerPrefs.GetInt("LevelPassed") < 4)
+                    PlayerPrefs.SetInt("LevelPassed", 4);
+                break;
+            case "Level 5":
+                if (PlayerPrefs.GetInt("LevelPassed") < 5)
+                    PlayerPrefs.SetInt("LevelPassed", 5);
+                break;
+            case "Level 6":
+                if (PlayerPrefs.GetInt("LevelPassed") < 6)
+                    PlayerPrefs.SetInt("LevelPassed", 6);
+                break;
+            case "Level 7":
+                if (PlayerPrefs.GetInt("LevelPassed") < 7)
+                    PlayerPrefs.SetInt("LevelPassed", 7);
+                break;
+            case "Level 8":
+                if (PlayerPrefs.GetInt("LevelPassed") < 8)
+                    PlayerPrefs.SetInt("LevelPassed", 8);
+                break;
+            case "Level 9":
+                if (PlayerPrefs.GetInt("LevelPassed") < 9)
+                    PlayerPrefs.SetInt("LevelPassed", 9);
+                break;
+            case "Level 10":
+                if (PlayerPrefs.GetInt("LevelPassed") < 10)
+                    PlayerPrefs.SetInt("LevelPassed", 10);
+                break;
+            case "Level 11":
+                if (PlayerPrefs.GetInt("LevelPassed") < 11)
+                    PlayerPrefs.SetInt("LevelPassed", 11);
+                break;
+            case "Level 12":
+                if (PlayerPrefs.GetInt("LevelPassed") < 12)
+                    PlayerPrefs.SetInt("LevelPassed", 12);
+                break;
         }
     }
 }
--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
+    public Button button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12;
+    private void Start()
+    {
+        SetActiveButtons();
+    }   
+
     public void SetFirstLevel()
     {
         SceneManager.LoadScene(2);
@@ -52,5 +59,31 @@ public class LevelSelector : MonoBehaviour
     public void SetTwelfthLevel()
     {
         SceneManager.LoadScene(13);
+    }
+
+    public void SetActiveButtons()
+    {
+        if (PlayerPrefs.GetInt("LevelPassed") > 0)
+            button2.interactable = true; 
+        if (PlayerPrefs.GetInt("LevelPassed") > 1)
+            button3.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 2)
+            button4.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 3)
+            button5.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 4)
+            button6.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 5)
+            button7.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 6)
+            button8.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 7)
+            button9.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 8)
+            button10.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 9)
+            button11.interactable = true;
+        if (PlayerPrefs.GetInt("LevelPassed") > 10)
+            button12.interactable = true;
     }
 }

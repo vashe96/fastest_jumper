d42965929457f4e14de9a5a6d87d39caaca195e9
Valentyn Shemchuk <valentinshemchuk8@gmail.com>
2020-06-07 13:31:24 +0300

    Level management

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void SetFirstLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void SetSecondLevel()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void SetThirdLevel()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void SetFourthLevel()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void SetFifthLevel()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void SetSixthLevel()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void SetSeventhLevel()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void SetEighthLevel()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void SetNinthLevel()
+    {
+        SceneManager.LoadScene(10);
+    }
+    public void SetTenthLevel()
+    {
+        SceneManager.LoadScene(11);
+    }
+    public void SetEleventhLevel()
+    {
+        SceneManager.LoadScene(12);
+    }
+    public void SetTwelfthLevel()
+    {
+        SceneManager.LoadScene(13);
+    }
+}

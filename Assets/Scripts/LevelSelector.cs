969f2925d28e23545e980f7db10606f5107a7262
Valentyn Shemchuk <valentinshemchuk8@gmail.com>
2020-06-08 18:46:42 +0300

    Minor

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class LevelSelector : MonoBehaviour
 {
     public Button button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12;
+    public Sprite b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
     private void Start()
     {
         SetActiveButtons();
@@ -69,27 +70,70 @@ public class LevelSelector : MonoBehaviour
     public void SetActiveButtons()
     {
         if (PlayerPrefs.GetInt("LevelPassed") >= 1)
-            button2.interactable = true; 
+        { 
+            button2.interactable = true;
+            button2.GetComponent<Image>().sprite = b2;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 2)
-            button3.interactable = true;
+        { 
+            button3.interactable = true; 
+            button3.GetComponent<Image>().sprite = b3;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 3)
+        { 
             button4.interactable = true;
+            button4.GetComponent<Image>().sprite = b4;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 4)
-            button5.interactable = true;
+        { 
+            button5.interactable = true; 
+            button5.GetComponent<Image>().sprite = b5;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 5)
-            button6.interactable = true;
+        { 
+            button6.interactable = true; 
+            button6.GetComponent<Image>().sprite = b6;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 6)
-            button7.interactable = true;
+        { 
+            button7.interactable = true; 
+            button7.GetComponent<Image>().sprite = b7;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 7)
-            button8.interactable = true;
+        { 
+            button8.interactable = true; 
+            button8.GetComponent<Image>().sprite = b8;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 8)
-            button9.interactable = true;
+        { 
+            button9.interactable = true; 
+            button9.GetComponent<Image>().sprite = b9;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 9)
+        {
             button10.interactable = true;
+            button10.GetComponent<Image>().sprite = b10;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 10)
+        {
             button11.interactable = true;
+            button11.GetComponent<Image>().sprite = b11;
+        }
+
         if (PlayerPrefs.GetInt("LevelPassed") >= 11)
-            button12.interactable = true;
+        { 
+            button12.interactable = true; 
+            button12.GetComponent<Image>().sprite = b12;
+        }
     }
 
     void CheckBackButton()

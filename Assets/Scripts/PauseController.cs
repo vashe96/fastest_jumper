9208b9ab137663ef174896cb5662c5be286fe9ab
Valentyn Shemchuk <valentinshemchuk8@gmail.com>
2020-06-14 16:28:39 +0300

    Pause functionality

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    public void SetPause()
+    {
+        pauseCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void SetUnpause()
+    {
+        pauseCanvas.SetActive(false);
+        Time.timeScale = 1;
+    }    
+}
